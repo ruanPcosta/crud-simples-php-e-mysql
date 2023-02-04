e51d692a62bb941b2ca25ff15beae7881ca01bc6
ruanPcosta <111321307+ruanPcosta@users.noreply.github.com>
2023-02-03 22:53:50 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,24 +1,27 @@
 <h1> Cadastrar um usuario </h1>
 <form method= "POST" action="cadastrar_action.php">
     <fieldset>
+        <legend>Campo de cadastro
+    <p>       
 <label>
      Seu nome: <input type="text" name= "nome"/>>
 
 </label>
+            </p>
 
-<label>
+    <p>        <label>
 Seu email: <input type="email" name="email">>
 
-</label>
+        </label> </p>
 
-<label>
+<p> <label>
     Sua senha: <input type="password" name="senha">>
-</label>
+    </label> </p>
 
-<label>
+<p>  <label>
     Data de nascimento: <input type="date" name="nascimento">>
-</label>
+    </label> </p>
 
-<input type="submit" value="Salvar infos"/>
-    </fieldset>
+<p><input type="submit" value="Salvar infos"/>
+            </fieldset> </p>
 </form>
